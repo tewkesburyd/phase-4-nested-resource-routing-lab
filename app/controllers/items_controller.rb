4026a229fd6fb30d190536b96684084c16bd67cd
David Tewkesbury <tewkesburyd@hotmail.com>
2023-01-13 21:45:15 -0700

    commit tree 2fd377596d20e2ceb7ed45a46bdba9b1c65329b8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    items = Item.find(params[:id])
+    render json: items, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    items = user.items.create(item_params)
+    render json: items, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: " not found" }, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+      user = User.find_by(id: params[:id])
+      render json: user, include: :items
   end
-
 end
